7f0cc5a4adafcd38798d74639706cb2644af52a9
alexey <63682890+Bulmenisaurus@users.noreply.github.com>
2023-11-25 21:48:18 -0800

    add a puzzle cache

--- src/Frontend/script.ts
+++ src/Frontend/script.ts
@@ -6,9 +6,16 @@ export interface Puzzle {
     rating: number;
 }
 
-const getPuzzle = async (rating: number) => {
+const getPuzzle = async (rating: number, pc: PuzzleCache) => {
+    const cached = pc.getCachedPuzzle();
+    if (cached !== null) {
+        return cached;
+    }
+
     const req = await fetch(`http://localhost:3000/puzzle?rating=${rating}`);
     const puzzle: Puzzle = await req.json();
+
+    pc.setCachedPuzzle(puzzle);
 
     return puzzle;
 };
@@ -29,6 +36,28 @@ const useRating = (): [() => number, (_: number) => void] => {
     return [getRating, setRating];
 };
 
+// cache the last shown puzzle to the user, so that if they reload the page, the same puzzle is shown
+// especially useful when you have hot-reloading enabled and any page refresh causes a puzzle to be requested
+class PuzzleCache {
+    getCachedPuzzle(): Puzzle | null {
+        const stored = localStorage.getItem('puzzleCache');
+
+        if (stored === null) {
+            return null;
+        } else {
+            return JSON.parse(stored);
+        }
+    }
+
+    setCachedPuzzle(puzzle: Puzzle) {
+        localStorage.setItem('puzzleCache', JSON.stringify(puzzle));
+    }
+
+    clearCachedPuzzle() {
+        localStorage.removeItem('puzzleCache');
+    }
+}
+
 const main = async () => {
     const [getRating, setRating] = useRating();
 
@@ -36,9 +65,11 @@ const main = async () => {
     const nextButton = document.getElementById('next-button')!;
     const title = document.getElementById('title')!;
 
+    const pc = new PuzzleCache();
+
     const chessboard = new ChessBoard(container);
 
-    const puzzle = await getPuzzle(getRating());
+    const puzzle = await getPuzzle(getRating(), pc);
 
     chessboard.loadPuzzle(puzzle);
     const fenToMove = puzzle.fen.split(' ')[1];
@@ -55,11 +86,13 @@ const main = async () => {
             setRating(getRating() - 10);
             console.log(`Expected line: ${chessboard.puzzleMoves.slice(1).join(' ')}`);
         }
+
+        pc.clearCachedPuzzle();
     });
 
     nextButton.onclick = async () => {
         nextButton.classList.remove('yes', 'no');
-        const puzzle = await getPuzzle(getRating());
+        const puzzle = await getPuzzle(getRating(), pc);
 
         chessboard.loadPuzzle(puzzle);
     };
